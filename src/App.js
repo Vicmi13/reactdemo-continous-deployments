3149d3a35e7e264ec9eaf70668a0d7756efc6c95
Victor <vicm_13@hotmail.com>
2019-11-05 13:20:03 -0600

    Change description

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <h2>React</h2>
         <img src={require("./one-punch.jpg")} className="App-logo" alt="logo" />
-        <p>React demo for continous deployments with aws codepipeline</p>
+        <p>React demo for one punch man</p>
       </header>
     </div>
   );
